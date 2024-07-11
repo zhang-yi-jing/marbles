73fa11c79144d492bce2d1d2ac8c20bdf34f4ea9
zhangyijing <1565593204h@gmail.com>
2024-07-11 17:17:57 +0800

    7.11

--- Assets/Script/CollisionDetector.cs
+++ Assets/Script/CollisionDetector.cs
@@ -4,11 +4,12 @@ public class CollisionDetector : MonoBehaviour
 {
     private bool isInTriggerRange = false;
     private Vector2 colliderPosition;
-    private float rotationSpeed = 180f; // 每秒旋转的角度
+    private float rotationSpeed; // 每秒旋转的角度
     private bool isRotating = false;
     private float triggerRadius = 0f; // 触发区域的半径
     private Rigidbody2D rb;
     private bool isShooting = false;
+    private float proximityPercentage;
 
     private void Start()
     {
@@ -23,7 +24,7 @@ public class CollisionDetector : MonoBehaviour
             colliderPosition = other.transform.position;
             triggerRadius = other.bounds.extents.x; // 获取触发物体的半径
             Debug.Log("Entered Trigger Range: " + other.gameObject.name + " at position: " + colliderPosition + ", Radius: " + triggerRadius);
-            
+
         }
     }
 
@@ -32,6 +33,7 @@ public class CollisionDetector : MonoBehaviour
         if (other.CompareTag("Trigger range"))
         {
             isInTriggerRange = false;
+            
         }
     }
 
@@ -40,25 +42,35 @@ public class CollisionDetector : MonoBehaviour
         if (isInTriggerRange && !isRotating && !isShooting)
         {
             CalculateProximityPercentage();
-
         }
         
 
         if (isInTriggerRange && Input.GetKeyDown(KeyCode.Space))
         {
-            isRotating = !isRotating;
             if (!isRotating)
             {
+                // 禁用物体上的Rigidbody组件
+                rb.isKinematic = true;
+                rb.velocity = Vector2.zero; // 将速度设置为零
+                RotateAroundColliderPosition();
+                Debug.Log(rb.velocity);
+                isShooting = false;
+            }
+
+            isRotating = !isRotating;
+
+            if (!isRotating)
+            {
+                rb.isKinematic = false;
+                rb.drag = 0.2f;
                 ApplyTangentVelocity();
                 isShooting = !isShooting;
-                rb.drag = 0.2f;
-            }
+            }   
         }
 
         if (isRotating)
         {
-            RotateAroundColliderPosition();
-            
+            RotateAroundColliderPosition();            
         }
     }
 
@@ -66,6 +78,20 @@ public class CollisionDetector : MonoBehaviour
     {
         // 获取挂载有该脚本的物体
         Transform thisTransform = transform;
+
+        if (proximityPercentage <= 0.35f)
+        {
+            rotationSpeed = 120f;
+        }
+        else if (proximityPercentage <= 0.55f)
+        {
+            rotationSpeed = 180f;
+        }
+        else
+        {
+            rotationSpeed = 240f;
+        }
+
 
         // 计算每帧应该旋转的角度
         float deltaAngle = rotationSpeed * Time.deltaTime;
@@ -80,7 +106,7 @@ public class CollisionDetector : MonoBehaviour
         float distance = Vector2.Distance(transform.position, colliderPosition);
 
         // 计算百分比
-        float proximityPercentage = 1f - (distance / triggerRadius);
+        proximityPercentage = 1f - (distance / triggerRadius);
 
         // 将百分比限制在0-100%之间
         proximityPercentage = Mathf.Clamp(proximityPercentage, 0f, 1f);
@@ -89,7 +115,7 @@ public class CollisionDetector : MonoBehaviour
         //Debug.Log("Proximity Percentage: " + (proximityPercentage * 100f) + "%");
 
         float linearDrag = Mathf.Lerp(5f, 15f, proximityPercentage);
-        Debug.Log(linearDrag);
+        //Debug.Log(linearDrag);
         rb.drag = linearDrag;
     }
 
@@ -99,7 +125,7 @@ public class CollisionDetector : MonoBehaviour
         Vector2 tangentDirection = new Vector2(transform.position.y - colliderPosition.y, colliderPosition.x - transform.position.x).normalized;
 
         // 应用切线速度
-        float tangentSpeed = 10f; // 切线速度大小
+        float tangentSpeed = 5f; // 切线速度大小
         rb.velocity = -tangentDirection * tangentSpeed;
     }
 }
