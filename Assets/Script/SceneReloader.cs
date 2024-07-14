08a9e8ffb33d3d937bc16fc2ea63d9df40248aed
zhangyijing <1565593204h@gmail.com>
2024-07-14 18:46:10 +0800

    7.14 18:46

--- Assets/Script/SceneReloader.cs
+++ Assets/Script/SceneReloader.cs
@@ -3,6 +3,19 @@ using UnityEngine.SceneManagement;
 
 public class SceneReloader : MonoBehaviour
 {
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            ReloadScene();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            ReturnToFirstScene();
+        }
+    }
+
     public void ReloadScene()
     {
         Time.timeScale = 1f;
@@ -11,7 +24,12 @@ public class SceneReloader : MonoBehaviour
 
         // 重新加载当前场景
         SceneManager.LoadScene(currentSceneIndex);
+    }
 
-        
+    public void ReturnToFirstScene()
+    {
+        Time.timeScale = 1f;
+        // 加载第一个场景（索引为 0）
+        SceneManager.LoadScene(0);
     }
 }
